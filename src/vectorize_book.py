afcdfe32d6ebac254e7609d781cdf0afb8f0a95a
m-disha1103 <disham492@gmail.com>
2026-09-22 14:29:37 +0530

    streamlit code

--- src/vectorize_book.py
+++ src/vectorize_book.py
@@ -15,7 +15,7 @@ data_dir = f"{parent_dir}/data"
 vector_db_dir = f"{parent_dir}/vector_db"
 chapters_vector_db_dir = f"{parent_dir}/chapters_vector_db"
 
-embedding = HuggingFaceEmbeddings(model_kwargs={"device": DEVICE}) # Use device from env
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={"device": cpu}) # Use device from env
 text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=500)
 
 
